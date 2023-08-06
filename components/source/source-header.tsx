71c034486d40af0a461dee8dc35132e46f4693dd
Derek Au <derek@derekau.net>
2023-08-06 00:01:38 -0400

    improve source icon classes

--- components/source/source-header.tsx
+++ components/source/source-header.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import { timeAgo } from '@/util/various';
 
 import type { BaseDocument } from '@/types/baseDocument';
+import { cn } from '@/lib/utils';
 
 interface CardSourceHeaderProps {
   item: BaseDocument;
@@ -16,21 +17,25 @@ export function SourceHeader({
   isSmall = false,
 }: CardSourceHeaderProps) {
   if (!item || !item.source) return null;
-  let containerClass = 'mb-2 flex items-center justify-between text-sm text-neutral-700 dark:text-neutral-400';
-  let imageContainerClass = 'relative mr-2 flex h-7 w-7 shrink-0 overflow-hidden rounded-full';
-  if (isSmall) {
-    containerClass = 'mb-2 flex items-center justify-between text-xs text-neutral-700 dark:text-neutral-400'
-    imageContainerClass = 'relative mr-2 flex h-6 w-6 shrink-0 overflow-hidden rounded-full';
-  }
 
   return (
-    <div className={containerClass}>
+    <div
+      className={cn(
+        'mb-2 flex items-center justify-between text-neutral-700 dark:text-neutral-400',
+        isSmall ? 'text-xs' : 'text-sm'
+      )}
+    >
       <Link
         href={`/?f=true&source=${item.source}`}
         className="inline-flex items-center"
       >
         {item.sourceId && (
-          <div className={imageContainerClass}>
+          <div
+            className={cn(
+              'relative mr-2 flex shrink-0 overflow-hidden rounded-full border border-neutral-300 dark:border-neutral-700',
+              isSmall ? 'h-6 w-6' : 'h-7 w-7'
+            )}
+          >
             <Image
               src={`/img/logos/${item.sourceId}.jpg`}
               className="aspect-square h-full w-full"
