50234da8a94dd93518b20e434e074d2acc77bbeb
Derek Au <derek@derekau.net>
2023-08-07 12:55:32 -0400

    refine term type title

--- components/search-card/term-card.tsx
+++ components/search-card/term-card.tsx
@@ -22,7 +22,7 @@ export function TermCard({ term }: { term: Term }) {
   return (
     <Link href={href}>
       <div className="bg-neutral-100 p-2 hover:bg-neutral-50 dark:bg-neutral-800 dark:hover:bg-neutral-700">
-        <h5 className="text-sm text-muted-foreground">{fieldName}</h5>
+        <h5 className="text-sm font-semibold text-muted-foreground">{fieldName}</h5>
         <h4 className="font-semibold">{term.value}</h4>
         {term?.summary && (
           <span className="text-sm text-muted-foreground">
