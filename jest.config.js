cd2d81305c3b9b1c0baf2974e9732edd95d6e432
Derek Philip Au <22045002+derekphilipau@users.noreply.github.com>
2023-10-13 10:03:03 -0400

    jest ignore e2e tests

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,7 @@ const createJestConfig = nextJest({ dir: './' });
 // Any custom config you want to pass to Jest
 const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  modulePathIgnorePatterns: ['<rootDir>/e2e/'],
 };
 
 // createJestConfig is exported in this way to ensure that next/jest can load the Next.js configuration, which is async
