bfec97f00749a1b3a58c7ce93afc89012e7d68a0
Derek Au <derek@derekau.net>
2023-11-07 22:15:12 -0500

    improve event card

--- components/search-card/event-card.tsx
+++ components/search-card/event-card.tsx
@@ -11,6 +11,7 @@ import {
 import type { LayoutType } from '@/lib/elasticsearch/search/searchParams';
 import { Icons } from '@/components/icons';
 import { SourceHeader } from '@/components/source/source-header';
+import { DocumentImage } from '../image/document-image';
 
 function getFormattedDate(event) {
   let startDate, endDate, formattedStartDate, formattedEndDate;
@@ -34,7 +35,7 @@ function getFormattedDate(event) {
     return `Starting ${formattedStartDate}`;
   }
   if (event.endDate) {
-    return `Until ${formattedEndDate}`;
+    return `Through ${formattedEndDate}`;
   }
 }
 
@@ -66,13 +67,13 @@ export function EventCard({
   const formattedDate = getFormattedDate(item);
 
   return (
-    <Link href={item.url}>
-      <div className={getContainerClass(layout)}>
-        <div>
-          {isMultiSource && layout === LAYOUT_GRID && (
-            <SourceHeader item={item} showDate={true} isSmall={true} />
-          )}
-          <div className="flex items-center justify-center bg-neutral-50 hover:bg-neutral-100 dark:bg-neutral-800 dark:hover:bg-neutral-700">
+    <div className={getContainerClass(layout)}>
+      <div>
+        {isMultiSource && layout === LAYOUT_GRID && (
+          <SourceHeader item={item} showDate={true} isSmall={true} />
+        )}
+        <div className="flex items-center justify-center bg-neutral-50 hover:bg-neutral-100 dark:bg-neutral-800 dark:hover:bg-neutral-700">
+          <Link href={item.url}>
             <figure>
               {item.image?.thumbnailUrl ? (
                 <Image
@@ -90,12 +91,14 @@ export function EventCard({
               )}
               <figcaption></figcaption>
             </figure>
-          </div>
+          </Link>
         </div>
-        <div className={getDetailsClass(layout)}>
-          {isMultiSource && layout !== LAYOUT_GRID && (
-            <SourceHeader item={item} />
-          )}
+      </div>
+      <div className={getDetailsClass(layout)}>
+        {isMultiSource && layout !== LAYOUT_GRID && (
+          <SourceHeader item={item} />
+        )}
+        <Link href={item.url}>
           {showType && layout === LAYOUT_LIST && (
             <h4 className="mb-2 text-base font-semibold uppercase text-neutral-500 dark:text-neutral-600">
               {dict['index.events.itemTitle']}
@@ -109,8 +112,8 @@ export function EventCard({
               {formattedDate}
             </p>
           )}
-        </div>
+        </Link>
       </div>
-    </Link>
+    </div>
   );
 }
