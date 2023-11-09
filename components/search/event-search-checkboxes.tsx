31363b0fab600dd25e4a5ac8854bb521266877fb
Derek Au <derek@derekau.net>
2023-11-09 15:33:51 -0500

    visx timeline feature

---
+++ components/search/event-search-checkboxes.tsx
@@ -0,0 +1,35 @@
+import { getDictionary } from '@/dictionaries/dictionaries';
+
+import type { SearchParams } from '@/lib/elasticsearch/search/searchParams';
+import { SearchCheckbox } from './search-checkbox';
+
+interface EventSearchCheckboxesProps {
+  params?: SearchParams;
+}
+
+export function EventSearchCheckboxes({ params }: EventSearchCheckboxesProps) {
+  const dict = getDictionary();
+
+  return (
+    <div className="flex flex-wrap gap-x-4 gap-y-2">
+      {/*
+      <div className="flex items-center space-x-2">
+        <SearchCheckbox
+          params={params}
+          name="isNow"
+          value={params?.isNow || false}
+          label={dict['search.isNow']}
+        />
+      </div>
+      */}
+      <div className="flex items-center space-x-2">
+        <SearchCheckbox
+          params={params}
+          name="tl"
+          value={params?.isShowTimeline || false}
+          label={dict['search.isShowTimeline']}
+        />
+      </div>
+    </div>
+  );
+}
