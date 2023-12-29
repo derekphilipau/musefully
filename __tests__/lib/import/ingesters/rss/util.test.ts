19ebdac51b3f7f669e3cb67637db40fa3f5d5364
Derek Au <derek@derekau.net>
2023-12-28 22:48:19 -0500

    rss import refine, edge cases

--- __tests__/lib/import/ingesters/rss/util.test.ts
+++ __tests__/lib/import/ingesters/rss/util.test.ts
@@ -1,16 +1,16 @@
 import fs from 'fs';
 import path from 'path';
-import { parseStringPromise } from 'xml2js';
 
 import {
   getRssItemId,
   getRssItemImageUrl,
   parseDate,
+  parseXml,
   transformRssItem,
 } from '@/lib/import/ingesters/rss/util';
 import { stripHtmlTags } from '@/lib/various';
 
-describe('rss import function', () => {
+describe('transformRssItem', () => {
   // Read the content of the RSS feed from the static file
   const testContent = fs.readFileSync(
     path.join(__dirname, './artnews.rss.xml'),
@@ -18,11 +18,33 @@ describe('rss import function', () => {
   );
 
   let items: any[] = [];
+  let nytItemXml: any = {};
 
   // Use beforeAll to process async operations before tests
   beforeAll(async () => {
-    const jsonObj = await parseStringPromise(testContent);
+    const jsonObj = await parseXml(testContent);
     items = jsonObj.rss.channel[0].item;
+
+    const nytItemRss = `
+    <item>
+      <title>How 1993 — and Two Watershed Shows — Help Make Sense of 2023</title>
+      <link>https://www.nytimes.com/2023/12/27/arts/design/1993-whitney-biennial-theater-of-refusal-art.html</link>
+      <guid isPermaLink="true">https://www.nytimes.com/2023/12/27/arts/design/1993-whitney-biennial-theater-of-refusal-art.html</guid>
+      <atom:link href="https://www.nytimes.com/2023/12/27/arts/design/1993-whitney-biennial-theater-of-refusal-art.html" rel="standout"/>
+      <description>A blue-chip gallery asks, does the infamous Whitney Biennial or “The Theater of Refusal” measure up 30 years later, when artists of color have moved to the mainstream?</description>
+      <dc:creator>Aruna D’Souza</dc:creator>
+      <pubDate>Wed, 27 Dec 2023 16:51:11 +0000</pubDate>
+      <category domain="http://www.nytimes.com/namespaces/keywords/des">Art</category>
+      <category domain="http://www.nytimes.com/namespaces/keywords/des">Photography</category>
+      <category domain="http://www.nytimes.com/namespaces/keywords/des">Race and Ethnicity</category>
+      <category domain="http://www.nytimes.com/namespaces/keywords/nyt_org">Hauser & Wirth</category>
+      <category domain="http://www.nytimes.com/namespaces/keywords/nyt_per">Fowle, Kate</category>
+      <category domain="http://www.nytimes.com/namespaces/keywords/nyt_geo">Los Angeles (Calif)</category>
+      <category domain="http://www.nytimes.com/namespaces/keywords/nyt_per">Gaines, Charles</category>
+      <category domain="http://www.nytimes.com/namespaces/keywords/nyt_org">Whitney Museum of American Art</category>
+      <category domain="http://www.nytimes.com/namespaces/keywords/nyt_org">University of California</category>
+    </item>`;
+    nytItemXml = (await parseXml(nytItemRss))?.item;
   });
 
   it('should return image for content embedded', () => {
@@ -34,7 +56,6 @@ describe('rss import function', () => {
 
   it("should return the rss item's id", () => {
     const itemId = getRssItemId(items[0]);
-    console.log(itemId);
     expect(itemId).toBe('https://www.artnews.com/?p=1234682122');
   });
 
@@ -51,6 +72,26 @@ describe('rss import function', () => {
     );
     expect(transformedItem.sourceId).toBe(sourceId);
     expect(transformedItem.title).toBe(strippedTitle);
-    expect(transformedItem.keywords).toBe('Art in America, Reviews');
+    expect(transformedItem.keywords).toEqual(['Art in America', 'Reviews']);
+  });
+
+  it('should return the transformed rss for nytimes', () => {
+    const sourceId = 'nytimes';
+    const transformedItem = transformRssItem(nytItemXml, sourceId);
+    expect(transformedItem.sourceId).toBe(sourceId);
+    expect(transformedItem.title).toBe(
+      'How 1993 — and Two Watershed Shows — Help Make Sense of 2023'
+    );
+    expect(transformedItem.keywords).toEqual([
+      'Art',
+      'Photography',
+      'Race and Ethnicity',
+      'Hauser & Wirth',
+      'Fowle, Kate',
+      'Los Angeles (Calif)',
+      'Gaines, Charles',
+      'Whitney Museum of American Art',
+      'University of California',
+    ]);
   });
 });
