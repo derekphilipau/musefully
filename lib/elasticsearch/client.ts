cb239af0c5752557e5a6b2a961fd997be85b19fb
Derek Au <derek@derekau.net>
2025-04-13 20:23:09 -0400

    don't require all env vars

--- lib/elasticsearch/client.ts
+++ lib/elasticsearch/client.ts
@@ -16,7 +16,7 @@ const useCloud = getEnvVar('ELASTICSEARCH_USE_CLOUD');
 const id = getEnvVar('ELASTICSEARCH_CLOUD_ID');
 const username = getEnvVar('ELASTICSEARCH_CLOUD_USERNAME');
 const password = getEnvVar('ELASTICSEARCH_CLOUD_PASSWORD');
-const localNode = getEnvVar('ELASTICSEARCH_LOCAL_NODE');
+const localNode = getEnvVar('ELASTICSEARCH_LOCAL_NODE', undefined);
 
 export function getClient(): Client {
   const clientConfig =
